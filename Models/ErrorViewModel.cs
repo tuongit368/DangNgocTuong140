33b087c20ccd62fccaf4470adc5646d6c7b4655a
TuongDay <tuongit368@gmail.com>
2024-01-06 11:09:15 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DangNgocTuong140.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
